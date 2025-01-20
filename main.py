a139fdd995351b5f404ad461d52e08a0387d54fa
bendruce <118769779+bendruce@users.noreply.github.com>
2025-01-20 15:31:48 +0000

    change message

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify(message="Hello, World! Welcome to COC105 CI/CD Lab!")
+    return jsonify(message="Hello, Cruel World! Welcome to COC105 CI/CD Lab!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
